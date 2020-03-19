60681f7aa7332ed4d7fc97652e7c0501aa4c57c1
Matt Redinger <mattred1@gmail.com>
2020-03-18 20:54:10 -0400

    Delete unnecessary commented out code

--- unit8/stalker-exercise/script.js
+++ unit8/stalker-exercise/script.js
@@ -5,18 +5,3 @@ var userAge = prompt("How old are you?");
 
 console.log("Nice to meet you, " + userFirstName + " " + userLastName);
 console.log("You are " + userAge + " years old.");
-
-
-
-// OR a cleaner way below...
-
-
-// var userFirstName = prompt("What is your first name?");
-// var userLastName = prompt("What is your last name?");
-// var userAge = prompt("How old are you?");
-
-// var fullName = userFirstName + " " + userLastName
-
-
-// console.log("Nice to meet you, " + fullName);
-// console.log("You are " + userAge + " years old.")
