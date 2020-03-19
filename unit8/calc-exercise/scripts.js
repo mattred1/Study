f7f7b0f0eba7d81520a9f00fa1e55e10c4810f2f
Matt Redinger <mattred1@gmail.com>
2020-03-18 20:52:10 -0400

    Create and complete age calculator exercise

---
+++ unit8/calc-exercise/scripts.js
@@ -0,0 +1,23 @@
+var age = prompt("How old are you?");
+var days = age * 365.25;
+// Add .25 to average for leap years
+alert(age + " years is roughly " + days + " days old");
+
+
+
+
+
+
+
+// OR a cleaner way below...
+
+
+// var userFirstName = prompt("What is your first name?");
+// var userLastName = prompt("What is your last name?");
+// var userAge = prompt("How old are you?");
+
+// var fullName = userFirstName + " " + userLastName
+
+
+// console.log("Nice to meet you, " + fullName);
+// console.log("You are " + userAge + " years old.")
