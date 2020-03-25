3144831445de23f0afa34a26a9b9290102dd4c80
Matt Redinger <mattred1@gmail.com>
2020-03-24 23:24:09 -0400

    complete exercises

---
+++ unit11/while.js
@@ -0,0 +1,88 @@
+// START TASK 1 
+/*
+var count = -10;
+
+while (count < 20) {
+    console.log(count);
+    count++
+}
+*/
+//END TASK 1
+
+
+
+
+
+// START TASK 2
+
+/*
+var count = 10;
+
+while (count < 41) {
+    console.log(count);
+    count+=2
+}
+*/
+
+
+//OR
+
+
+// -- This one is not as efficient because it runs more times in the console, however it's also actually checking if it's even
+/*
+var count = 10;
+
+while (count < 41) {                           //Run as long as ccount is less than 41
+    if (count % 2 === 0) {                           //This is saying if count is divisible by 2 and has a remainder of 0 (basically if it's even)
+        console.log(count)
+    }
+    count ++;
+}
+*/
+
+//END TASK 2
+
+
+
+
+
+
+
+
+// START TASK 3
+
+/*
+var count = 300;
+
+while (count < 334) {
+    if (count % 2 === 1) {                      //Could also do 2 !== 0 
+        console.log(count)
+    }
+    count++;
+}
+*/
+//END TASK 3
+
+
+
+
+
+
+
+// START TASK 4
+/*
+var count = 5;
+
+while (count < 51) {
+    if (count % 5 === 0  && count % 3 === 0) {                      // Divisible by both 5 AND 3
+        console.log(count)
+    }
+    count++;
+}
+*/
+
+//END TASK 4
+
+
+
+
