809e7124836644d5ee75f413cf7fdfe5900aa8ae
Matt Redinger <mattred1@gmail.com>
2020-05-14 22:33:29 -0400

    Refactor: deleteTodo()
    - use prompt to ask user index of element to delete
    - splice that element
    - log confirmation that element was deleted

--- unit13/list.js
+++ unit13/list.js
@@ -15,12 +15,7 @@ while (input !== "quit") {                                              // as lo
     } else if (input === "new") {
         addTodo();
     } else if (input === "delete") {
-        // ask for index of todo to be deleted
-        var index = prompt("Enter index of todo to delete");
-        
-        // delete that todo
-        todos.splice(index,1);                                                     //remove index that user provides (index is what i chose to call it for ease). after comma is number of elements to delete
-        console.log("Deleted Todo!");
+        deleteTodo();
     }
 
     //ask again for new input
@@ -50,6 +45,13 @@ function addTodo() {
 }
 
 
+function deleteTodo() {
+    // ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+    // delete that todo
+    todos.splice(index, 1);                                                     //remove index that user provides (index is what i chose to call it for ease). after comma is number of elements to delete
+    console.log("Deleted Todo!");
+}
 
 
 
