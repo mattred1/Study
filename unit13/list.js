2a1627e89bcc7132dddeb785621548e4b5974f30
Matt Redinger <mattred1@gmail.com>
2020-05-14 22:10:45 -0400

    add functionality to delete array elements using splice using element's index

--- unit13/list.js
+++ unit13/list.js
@@ -19,6 +19,12 @@ while (input !== "quit") {                                              // as lo
     } else if (input === "new") {
         var newTodo = prompt("Enter new todo");        // if input is equal to new, add new todo to end of array (push)
         todos.push(newTodo);
+    } else if (input === "delete") {
+        // ask for index of todo to be deleted
+        var index = prompt("Enter index of todo to delete");
+        
+        // delete that todo
+        todos.splice(index,1);                                                     //remove index that user provides (index is what i chose to call it for ease). after comma is number of elements to delete
     }
 
     //ask again for new input
