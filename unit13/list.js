c93d247c79119d81c30a00a0ef9540de2a24106e
Matt Redinger <mattred1@gmail.com>
2020-05-14 21:59:57 -0400

    add asterisks around logged array items

--- unit13/list.js
+++ unit13/list.js
@@ -11,9 +11,11 @@ var input = prompt("What would you like to do?");
 while (input !== "quit") {                                              // as long as input doesn't equal "quit", continue running this loop
     //handle input
     if (input === "list") {
+        console.log("**********")                                                   // Simple - adds *'s around our list. Since it's outside of the forEach loop, it doesn't repeat or anything.
         todos.forEach(function(todo, index) {                          // use forEach loop to log each todo (function name randomly chosen - it's just applicable here). "index" is the 2nd argument a forEach loop will take. I chose to stick to that name (you can change it to whatever you want - doesn't have to be index)
             console.log(index + ": "  + todo);                                          // if given the command "list," console.log each todo. forEach will log each todo on a separate line. Will print index + ": " + name of todo
         });
+        console.log("**********")
     } else if (input === "new") {
         var newTodo = prompt("Enter new todo");        // if input is equal to new, add new todo to end of array (push)
         todos.push(newTodo);
