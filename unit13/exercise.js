6eae4b73041b5d3e76b7f673be8285460f9db83f
Matt Redinger <mattred1@gmail.com>
2020-05-17 16:08:45 -0400

    Finish exercise 1

--- unit13/exercise.js
+++ unit13/exercise.js
@@ -1,4 +1,19 @@
+
 console.log("Connected!");
+
+
+
+/*
+    function printReverse(arr) {
+        for(var i = arr.length - 1; i >= 0; i--) {                                                                //arr.length will get the length of the array, and we add "-1" because the index is always one less than length
+            console.log(arr[i]);                                                                                          //print out array of i(starting with the last element, per i=arr.length)
+        }
+    }
+
+    printReverse([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);                                                                //runs the array as an argument. open the console to look.
+*/
+
+
 
 
 
