988d2b0c74ecab783f43267cfc6e7815492e82cf
Matt Redinger <mattred1@gmail.com>
2020-07-12 20:10:38 -0400

    Turn each <li> pink
    use addEventListener to run through <li>'s and turn each clicked <li> pink

--- unit16/domdemo.js
+++ unit16/domdemo.js
@@ -1,1 +1,8 @@
-alert("sup");
+//Want something to happen when I click on an individual <li> by attaching a separate listener to each <li>
+var lis = document.querySelectorAll("li");
+
+for(var i = 0; i < lis.length; i++) {  // this starts at the first li, and loops through each <li> - the full length of the li's
+    lis[i].addEventListener("click", function() {
+        this.style.color = "pink";                 //this in addEventListener refers to the item that was clicked on, in this case "lis[i]"
+    })
+};
