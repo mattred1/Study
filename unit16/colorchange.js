90636d350d89dcd32182af76ad1724954c47810c
Matt Redinger <mattred1@gmail.com>
2020-07-23 22:02:59 -0400

    Refactor button toggling background change

--- unit16/colorchange.js
+++ unit16/colorchange.js
@@ -1,14 +1,19 @@
 var button = document.querySelector("button");
 var darkMode = false;
 
+// button.addEventListener("click", function() {
+//     if (darkMode) {
+//         document.body.style.background = "white";
+//         button.style.background = "#EFEFEF";
+//     } else {
+//         document.body.style.background = "#212121";
+//         button.style.background = "red";
+//     }
+//     darkMode = !darkMode;
+// });
+
+
+// Refactored, like WOAH
 button.addEventListener("click", function() {
-    if (darkMode) {
-        document.body.style.background = "white";
-        button.style.background = "#EFEFEF";
-        darkMode = false;
-    } else {
-        document.body.style.background = "#212121";
-        button.style.background = "red";
-        darkMode = true;
-    }
+    document.body.classList.toggle("getDark");
 });
