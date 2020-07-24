9f90b2a0fb69297fa48e0094c2e6451e356d9b15
Matt Redinger <mattred1@gmail.com>
2020-07-23 22:35:30 -0400

    Create butttons and input; Add eventListeners to buttons
    Incrementally increase score and display

--- unit16/scores.js
+++ unit16/scores.js
@@ -1,1 +1,18 @@
-alert("yo");
+var p1Button = document.getElementById("p1");
+var p2Button = document.querySelector("#p2");
+var p1Score = 0; // scores start at 0 obviously
+var p2Score = 0;
+var p1Display = document.getElementById("p1Display"); // need a way to display scores and update as buttons are clicked
+var p2Display = document.getElementById("p2Display");
+
+// selects player one button and incrementally updates score as button is clicked, then displays it
+p1Button.addEventListener("click", function() {
+    p1Score++; // update score
+    p1Display.textContent = p1Score; // then display the value of the score
+});
+
+
+p2Button.addEventListener("click", function() {
+    p2Score++;
+    p2Display.textContent = p2Score;
+});
