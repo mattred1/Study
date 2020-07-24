e75f6a20422163f1e16873b1952f013da2b96359
Matt Redinger <mattred1@gmail.com>
2020-07-23 21:52:02 -0400

    Make button toggle background/button color

--- unit16/colorchange.js
+++ unit16/colorchange.js
@@ -1,5 +1,14 @@
 var button = document.querySelector("button");
+var darkMode = false;
 
 button.addEventListener("click", function() {
-    document.body.style.background = "blue";
+    if (darkMode) {
+        document.body.style.background = "white";
+        button.style.background = "#EFEFEF";
+        darkMode = false;
+    } else {
+        document.body.style.background = "#212121";
+        button.style.background = "red";
+        darkMode = true;
+    }
 });
