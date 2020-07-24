2634e0483f9ae1f74b9edb763e71b2027f251f12
Matt Redinger <mattred1@gmail.com>
2020-07-23 22:47:30 -0400

    Move player score incrementing into if statements
    Updating the scores only run as long as gameOver is false

--- unit16/scores.js
+++ unit16/scores.js
@@ -4,15 +4,33 @@ var p1Score = 0; // scores start at 0 obviously
 var p2Score = 0;
 var p1Display = document.getElementById("p1Display"); // need a way to display scores and update as buttons are clicked
 var p2Display = document.getElementById("p2Display");
+var gameOver = false;
+var winningScore = 5;
+
 
 // selects player one button and incrementally updates score as button is clicked, then displays it
 p1Button.addEventListener("click", function() {
-    p1Score++; // update score
-    p1Display.textContent = p1Score; // then display the value of the score
+    if (!gameOver){
+        p1Score++; // update score
+        if(p1Score === winningScore) { // once p1Score ==== 5, gameOver becomes set to true, and this whole function is an if statement for if it's NOT game over
+            gameOver = true;
+        }
+        p1Display.textContent = p1Score; // then display the value of the score
+    }
+
 });
 
 
 p2Button.addEventListener("click", function() {
-    p2Score++;
-    p2Display.textContent = p2Score;
+    if(!gameOver) {
+        p2Score++;
+        if (p2Score === winningScore){
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    
+    }
+
+
+
 });
