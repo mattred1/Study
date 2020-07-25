06761ba4b42eee8b6b603f48d1cb62ed12d7f448
Matt Redinger <mattred1@gmail.com>
2020-07-24 22:43:20 -0400

    Add winner class to whoever wins
    adds green text

--- unit16/scores.js
+++ unit16/scores.js
@@ -1,5 +1,6 @@
 var p1Button = document.getElementById("p1");
 var p2Button = document.querySelector("#p2");
+var resetButton = document.getElementById("reset");
 var p1Score = 0; // scores start at 0 obviously
 var p2Score = 0;
 var p1Display = document.getElementById("p1Display"); // need a way to display scores and update as buttons are clicked
@@ -13,6 +14,7 @@ p1Button.addEventListener("click", function() {
     if (!gameOver){
         p1Score++; // update score
         if(p1Score === winningScore) { // once p1Score ==== 5, gameOver becomes set to true, and this whole function is an if statement for if it's NOT game over
+            p1Display.classList. add("winner");
             gameOver = true;
         }
         p1Display.textContent = p1Score; // then display the value of the score
@@ -25,12 +27,10 @@ p2Button.addEventListener("click", function() {
     if(!gameOver) {
         p2Score++;
         if (p2Score === winningScore){
+            p2Display.classList. add("winner");
             gameOver = true;
         }
         p2Display.textContent = p2Score;
-    
     }
-
-
 
 });
