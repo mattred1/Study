5ec6f69d0820ddd121c592c436a125ee36c03557
Matt Redinger <mattred1@gmail.com>
2020-07-24 23:11:23 -0400

    Reorganized variables and separated
    Separating based on ones created vs ones that are being manipulated

--- unit16/scores.js
+++ unit16/scores.js
@@ -1,10 +1,12 @@
 var p1Button = document.getElementById("p1");
 var p2Button = document.querySelector("#p2");
 var resetButton = document.getElementById("reset");
-var p1Score = 0; // scores start at 0 obviously
-var p2Score = 0;
 var p1Display = document.getElementById("p1Display"); // need a way to display scores and update as buttons are clicked
 var p2Display = document.getElementById("p2Display");
+var numInput = document.querySelector("input[type='number]"); // don't need to specify that type = number, but doesn't hurt to be specific
+
+var p1Score = 0; // scores start at 0 obviously
+var p2Score = 0;
 var gameOver = false;
 var winningScore = 5;
 
