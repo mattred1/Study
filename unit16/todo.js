7c1dd426d676d8273000b5edb33d23b11f7cc326
Matt Redinger <mattred1@gmail.com>
2020-07-28 21:31:53 -0400

    Make hover effect cause 2rem font and green background

--- unit16/todo.js
+++ unit16/todo.js
@@ -2,6 +2,12 @@ var firstLi = document.querySelector("li");
 
 //only happens on initial mouseover/hover. Stays green though
 firstLi.addEventListener("mouseover", function() { 
-    firstLi.style.color = "green";
+    firstLi.style.backgroundColor = "green";
+    firstLi.style.fontSize = "2rem";
 });
 
+firstLi.addEventListener("mouseout", function() {
+    firstLi.style.backgroundColor = "white";
+    firstLi.style.fontSize = "1rem";
+
+});
