bdc7ea608718a36418cb818cec7c3218bb18aa04
Matt Redinger <mattred1@gmail.com>
2020-07-28 21:40:05 -0400

    Create over effect for all li's
    Create for loop to loop over all li's

--- unit16/todo.js
+++ unit16/todo.js
@@ -1,13 +1,27 @@
-var firstLi = document.querySelector("li");
+var allLis = document.querySelectorAll("li");
 
 //only happens on initial mouseover/hover. Stays green though
-firstLi.addEventListener("mouseover", function() { 
-    firstLi.style.backgroundColor = "green";
-    firstLi.style.fontSize = "2rem";
-});
+for (var i = 0; i < allLis.length; i++) {
+    allLis[i].addEventListener("mouseover", function() { // here, this refers to the element that the event was triggered on. Easy reference
+        this.style.color = "green";
+    });
+    allLis[i].addEventListener("mouseout", function() {
+        this.style.color = "black";
+    });
 
-firstLi.addEventListener("mouseout", function() {
-    firstLi.style.backgroundColor = "white";
-    firstLi.style.fontSize = "1rem";
+}
 
-});
+
+
+
+//This was used for only the top <li>. In order to have the hover effect for all <li>'s we had to use a loop.
+// firstLi.addEventListener("mouseover", function() { 
+//     firstLi.style.backgroundColor = "green";
+//     firstLi.style.fontSize = "2rem";
+// });
+
+// firstLi.addEventListener("mouseout", function() {
+//     firstLi.style.backgroundColor = "white";
+//     firstLi.style.fontSize = "1rem";
+
+// });
