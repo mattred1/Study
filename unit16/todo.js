f06e9b07cc8289787a2af7e605156581d21e831c
Matt Redinger <mattred1@gmail.com>
2020-07-28 21:26:26 -0400

    Create mouseover color change effect

---
+++ unit16/todo.js
@@ -0,0 +1,7 @@
+var firstLi = document.querySelector("li");
+
+//only happens on initial mouseover/hover. Stays green though
+firstLi.addEventListener("mouseover", function() { 
+    firstLi.style.color = "green";
+});
+
