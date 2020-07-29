2993ff9cb9de50c5c95b0671ce7125406abb0dcc
Matt Redinger <mattred1@gmail.com>
2020-07-28 21:44:11 -0400

    Create click event; toggle class

--- unit16/todo.js
+++ unit16/todo.js
@@ -2,11 +2,17 @@ var allLis = document.querySelectorAll("li");
 
 //only happens on initial mouseover/hover. Stays green though
 for (var i = 0; i < allLis.length; i++) {
+
     allLis[i].addEventListener("mouseover", function() { // here, this refers to the element that the event was triggered on. Easy reference
         this.style.color = "green";
     });
+
     allLis[i].addEventListener("mouseout", function() {
         this.style.color = "black";
+    });
+
+    allLis[i].addEventListener("click", function() {
+        this.classList.toggle("done");
     });
 
 }
