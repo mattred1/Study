d31d4cf349fe93f5187dad62f9f641b822a0b22c
Matt Redinger <mattred1@gmail.com>
2020-08-18 23:05:13 -0400

    Add event listener to compare clickedColor to pickedColor

--- unit17/colorgame.js
+++ unit17/colorgame.js
@@ -8,15 +8,36 @@ var colors = [
     "rgb(0, 255, 255)"
 ]
 
+
 // Selects class square. Needs "." because of type of selector
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[5];
 var colorDisplay = document.getElementById("colorDisplay");
 
+
+
 // Displays color in h1 that was picked (only colors[5] for now)
 colorDisplay.textContent = pickedColor;
 
+
+
 // Targets .squares and changes background to equal colors. Loops through array of colors
 for(var i = 0; i < squares.length; i++) {
+    // Add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    // Add click listeners to squares
+    squares[i].addEventListener("click", function() {
+        // grab color of picked square
+        var clickedColor = this.style.backgroundColor;
+        // compare color to pickedColor
+        if(clickedColor === pickedColor) {
+            alert("correct");
+        } else {
+            alert("wrong");
+        } 
+    });
+
 }
+
+
