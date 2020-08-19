67332cd5750c9c8be41054017114acfe2730d201
Matt Redinger <mattred1@gmail.com>
2020-08-18 22:43:15 -0400

    Add comments for notes

--- unit17/colorgame.js
+++ unit17/colorgame.js
@@ -1,3 +1,4 @@
+// Will go back later to randomly pick 3 numbers 0-255 to get random rgb colors
 var colors = [
     "rgb(255, 0, 0)",
     "rgb(0, 255, 0)",
@@ -9,6 +10,7 @@ var colors = [
 
 var squares = document.querySelectorAll(".square");
 
+// Targets .squares and changes background to equal colors. Loops through array of colors
 for(var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
 }
