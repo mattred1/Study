6f6d248ccc1fd0d1a578b1e8ec2171d020a1b213
Matt Redinger <mattred1@gmail.com>
2020-08-22 17:01:16 -0400

    Select h1 and change color to match clickedColor/winning color

--- unit17/colorgame.js
+++ unit17/colorgame.js
@@ -6,6 +6,7 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 
 // Displays color in h1 that was picked (only colors[5] for now)
@@ -26,6 +27,7 @@ for(var i = 0; i < squares.length; i++) {
         if(clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!"
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try again"
