af715ec1dd7543f43e83ddfb51fc452d9a1fd9f9
Matt Redinger <mattred1@gmail.com>
2020-10-06 22:41:25 -0400

    Add "click 'x' to remove" functionality

--- unit20/assets/js/scripts.js
+++ unit20/assets/js/scripts.js
@@ -1,3 +1,15 @@
+// Click li to gray out/crossoff
 $("li").click(function() {
     $(this).toggleClass("completed");
 });
+
+
+
+// Click on X to delete todo
+$("span").click(function(event) {
+    $(this).parent().fadeOut(600, function() { //removes entire parent, aka "li"
+        $(this).remove(); //refers to parent, since it's refering to "this" aka basically what it's contained in
+    }); 
+    event.stopPropagation();
+});
+//event bubbling - event bubbles up to parent elements. in this case, clicking "span" is ALSO clicking "li", which is ALSO clicking "ul" and "#container" and "body" etc
