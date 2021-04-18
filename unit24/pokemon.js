98d70cb62623e7503c1c231a8d17b908badc6381
Matt Redinger <mattred1@gmail.com>
2021-04-18 14:28:05 -0400

    Add image through js

---
+++ unit24/pokemon.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('#container');
+const newImg = document.createElement('img');
+
+newImg.src= 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg';
+container.appendChild(newImg);
