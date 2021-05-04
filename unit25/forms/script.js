d1f159036bdfc63ebc37a020ace05c12ffd29a52
Matt Redinger <mattred1@gmail.com>
2021-05-04 19:59:19 -0400

    Refactor code
    Create addTweet function

--- unit25/forms/script.js
+++ unit25/forms/script.js
@@ -7,16 +7,16 @@ tweetForm.addEventListener('submit', function(e) {
     const username = tweetForm.elements.username.value
 	const tweet = tweetForm.elements.tweet.value
 
+    addTweet(username, tweet);
+
+})
+
+const addTweet = (username, tweet) => {
 	const newTweet = document.createElement('li');
 	const bTag = document.createElement('b');
 
 	bTag.append(username);
 	newTweet.append(bTag);
-	newTweet.append(`- ${tweet}`);
+	newTweet.append(`: ${tweet}`);
     tweetContainer.append(newTweet);
-
-})
-
-//there is a request being made behind the scene that forces us to the next page
-//we want to change the default action
-//We can't just remove 'action="/dogs"' because that would just refresh the page
+}
